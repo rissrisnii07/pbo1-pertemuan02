6e25274086a9ddf7515a37174a8cce2467fa05bb
rissrisnii07 <155290609+rissrisnii07@users.noreply.github.com>
2024-04-19 23:25:28 +0800

    Add files via upload

---
+++ pbo1-pertemuan02/model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ===================");
+  System.out.println("NIDN : 2210010104");
+  System.out.println("Nama : Risnianti Nurkhalifah");
+  System.out.println("=====================================");
+ }
+}
